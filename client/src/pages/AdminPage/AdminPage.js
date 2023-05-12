3c1947ca4028f4ac42c943959a660913babc84f0
Evgeniy <zhrkacomua@gmail.com>
2023-05-12 23:37:29 +0300

    add: create admin dashboard

---
+++ client/src/pages/AdminPage/AdminPage.js
@@ -0,0 +1,49 @@
+import React, {useState} from 'react';
+import './AdminPage.scss';
+import Dashboard from "../../components/Dashboard/Dashboard";
+
+const IsAdminFrom = ({setIsAdmin}) => {
+    const [password, setPassword] = useState('');
+
+    const adminPassword = '123123123';
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setIsAdmin(adminPassword === password);
+    };
+    return (
+        <>
+            <div className="login">
+                <div className="login_container">
+                    <div className="login_container__title"></div>
+                    <form className="login_container__form"
+                          onSubmit={handleSubmit}>
+                        <input
+                            className="password"
+                            type="password"
+                            id="admin-password"
+                            value={password}
+                            placeholder="Password"
+                            onChange={(e) => setPassword(e.target.value)}
+
+                        />
+                        <button className="btnSubmit">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </>
+    );
+};
+
+const AdminPage = () => {
+    const [isAdmin, setIsAdmin] = useState(false);
+    if (!isAdmin) return <IsAdminFrom setIsAdmin={setIsAdmin}/>;
+
+    return (
+        <>
+            <Dashboard/>
+        </>
+    );
+};
+
+export default AdminPage;
