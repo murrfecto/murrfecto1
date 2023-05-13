5924b08fee605400fa323feb55e0e365f77e9469
Tonya-Shyva <toshash69@gmail.com>
2023-05-13 20:20:31 +0300

    done 404Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,25 @@
 import Main from "./pages/Main/Main";
-import './global.css'
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import "./global.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import {Admin,Home} from "./pages";
+import { Admin, Home } from "./pages";
+import Page404 from "./pages/Page404/Page404";
 
 function App() {
-    return (
-        <div className="App">
-            <BrowserRouter>
-                <Header/>
-                <Routes>
-                    <Route path='/' element={<Home/>}/>
-                    <Route path='/admin/*' element={<Admin/>}/>
-                </Routes>
-                <Footer/>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/admin/*" element={<Admin />} />
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
