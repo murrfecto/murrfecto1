682445efa1a31f787d41da6a89a93c8d238524a0
Tonya-Shyva <toshash69@gmail.com>
2023-05-16 09:16:55 +0300

    OurCats as list
    div=>ul,li;
    useEffect+[ ];
    CatCatds +prop key;

--- client/src/components/CatCards/CatCards.js
+++ client/src/components/CatCards/CatCards.js
@@ -1,48 +1,48 @@
-import React, {useState} from "react";
-import {Skeleton} from "@mui/material";
+import React, { useState } from "react";
+import { Skeleton } from "@mui/material";
 import "./CatCards.scss";
 import paw from "../../assets/paw.svg";
 import info from "../../assets/info-rounded.svg";
 
-const CatCards = ({src, alt, name, description, chippedInfo}) => {
-    const [loading, setLoading] = useState(true);
+const CatCards = ({ src, alt, name, description, chippedInfo }) => {
+  const [loading, setLoading] = useState(true);
 
-    const handleImageLoad = () => {
-        setLoading(false);
-    };
+  const handleImageLoad = () => {
+    setLoading(false);
+  };
 
-    return (
-        <div className={"cat_card"}>
-            {loading ? (
-                <Skeleton
-                    variant="rect"
-                    style={{borderRadius: "20px"}}
-                    width={305}
-                    height={295}
-                />
-            ) : (
-                <img
-                    className={"cat_card-icon"}
-                    src={src}
-                    alt={alt}
-                    onLoad={handleImageLoad}
-                />
-            )}
-            <div className={"cat_card-wrapper"}>
-                <div className={"cat_card-name"}>
-                    <p>{name}</p>
-                </div>
-                <div className={"cat_card-description"}>
-                    <img src={paw} alt="paw"/>
-                    <p>{description}</p>
-                </div>
-                <div className={"cat_card-chipped"}>
-                    <img src={info} alt="info"/>
-                    <p>{chippedInfo}</p>
-                </div>
-            </div>
+  return (
+    <li className={"cat_card"}>
+      {loading ? (
+        <Skeleton
+          variant="rect"
+          style={{ borderRadius: "20px" }}
+          width={305}
+          height={295}
+        />
+      ) : (
+        <img
+          className={"cat_card-icon"}
+          src={src}
+          alt={alt}
+          onLoad={handleImageLoad}
+        />
+      )}
+      <div className={"cat_card-wrapper"}>
+        <div className={"cat_card-name"}>
+          <p>{name}</p>
         </div>
-    );
+        <div className={"cat_card-description"}>
+          <img src={paw} alt="paw" />
+          <p>{description}</p>
+        </div>
+        <div className={"cat_card-chipped"}>
+          <img src={info} alt="info" />
+          <p>{chippedInfo}</p>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default CatCards;
--- client/src/components/OurCats/OurCats.js
+++ client/src/components/OurCats/OurCats.js
@@ -1,38 +1,42 @@
-import React, {useEffect, useState} from 'react';
-import './OurCats.scss'
+import React, { useEffect, useState } from "react";
+import "./OurCats.scss";
 import CatCards from "../CatCards/CatCards";
 import axios from "axios";
 
-
 const OurCats = () => {
-    const [cats, setCats] = useState(null);
-    const getData = async () => {
-        try {
-            const response = await axios.get('http://localhost:3000/cats')
-            setCats(response.data)
-        } catch (e) {
-            console.log(e.message)
-        }
+  const [cats, setCats] = useState(null);
+  const getData = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/cats");
+      setCats(response.data);
+    } catch (e) {
+      console.log(e.message);
     }
-    useEffect(() => {
-        getData()
-    })
-    return (
-        <article>
-            <h2>Наші хвости</h2>
-            <div className={'cats_cards'}>
-                {cats?.map((cat) => (
-                    <CatCards
-                        src={cat?.image}
-                        alt={cat?.name}
-                        name={cat?.name}
-                        description={cat?.description}
-                        chippedInfo={cat?.chipped}/>
-                ))}
-            </div>
-            <a className={'cats_cards-link'} href="#">Переглянути усіх</a>
-        </article>
-    );
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <article>
+      <h2>Наші хвости</h2>
+      <ul className={"cats_cards"}>
+        {cats?.map((cat) => (
+          <CatCards
+            key={cat?._id}
+            src={cat?.image}
+            alt={cat?.name}
+            name={cat?.name}
+            description={cat?.description}
+            chippedInfo={cat?.chipped}
+          />
+        ))}
+      </ul>
+      <a className={"cats_cards-link"} href="#">
+        Переглянути усіх
+      </a>
+    </article>
+  );
 };
 
 export default OurCats;
