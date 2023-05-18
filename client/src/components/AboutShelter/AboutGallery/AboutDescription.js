73952e686bf00a7c6b6e1b36b60e6d23c6fe6df2
Tonya-Shyva <toshash69@gmail.com>
2023-05-18 13:52:24 +0300

    add mobile images
    correct width for img

--- client/src/components/AboutShelter/AboutGallery/AboutDescription.js
+++ client/src/components/AboutShelter/AboutGallery/AboutDescription.js
@@ -3,6 +3,10 @@ import deskFirst from "../../../assets/about-page/desktop/about-one-min.jpg";
 import deskSecond from "../../../assets/about-page/desktop/about-two-min.jpg";
 import deskThird from "../../../assets/about-page/desktop/about-three-min.jpg";
 
+import mobFirst from "../../../assets/about-page/mobile/mobFirst.jpg";
+import mobSecond from "../../../assets/about-page/mobile/mobSecond.jpg";
+import mobThird from "../../../assets/about-page/mobile/mobThird.jpg";
+
 const AboutDescription = () => {
   return (
     <section className="about_description">
@@ -10,11 +14,12 @@ const AboutDescription = () => {
         <ul className="list">
           <li className="item">
             <picture>
-              <source srcSet={deskFirst} media="(min-width: 390px)" />
+              <source srcSet={mobFirst} media="(min-width: 390px)" />
+              <source srcSet={deskFirst} media="(min-width: 1440px)" />
 
               <img
                 className="img"
-                src={deskFirst}
+                src={mobFirst}
                 alt="Дівчина грається з котом"
                 width={359}
               />
@@ -26,14 +31,10 @@ const AboutDescription = () => {
           </li>
           <li className="item">
             <picture>
-              <source srcSet={deskSecond} media="(min-width: 390px)" />
+              <source srcSet={mobSecond} media="(min-width: 390px)" />
+              <source srcSet={deskSecond} media="(min-width: 1440px)" />
 
-              <img
-                className="img"
-                src={deskSecond}
-                alt="Три коти"
-                width={359}
-              />
+              <img className="img" src={mobSecond} alt="Три коти" width={359} />
             </picture>
             <p className="description_text">
               Є багато котів, які живуть прайдом в більш-менш безпечному місці,
@@ -43,9 +44,15 @@ const AboutDescription = () => {
           </li>
           <li className="item">
             <picture>
-              <source srcSet={deskThird} media="(min-width: 390px)" />
+              <source srcSet={mobThird} media="(min-width: 390px)" />
+              <source srcSet={deskThird} media="(min-width: 1440px)" />
 
-              <img className="img" src={deskThird} alt="Три коти" width={359} />
+              <img
+                className="img"
+                src={mobThird}
+                alt="Котик їсть"
+                width={359}
+              />
             </picture>
             <p className="description_text">
               Ми поєднуємо одних з другими, приймаючи від одних щомісячну
