3a4dde49a14528a19858783f5ad658f016b86830
Tonya-Shyva <toshash69@gmail.com>
2023-05-19 12:21:59 +0300

    Statistic mob&desktop styles

---
+++ client/src/components/AboutShelter/AboutGallery/StatisticDatas/StatisticDatas.js
@@ -0,0 +1,41 @@
+import "./StatisticDatas.scss";
+import "../AboutDescription.scss";
+import { ReactComponent as HeartIcon } from "../../../../assets/about-page/statistic/icon-heart.svg";
+import { ReactComponent as MoneyIcon } from "../../../../assets/about-page/statistic/icon-money.svg";
+import { ReactComponent as HandsIcon } from "../../../../assets/about-page/statistic/icon-hands.svg";
+
+const StatisticDatas = () => {
+  return (
+    <section className="section">
+      <div className="container">
+        <ul className="statistic_list">
+          <li className="statistic_item">
+            <div className="icon_wrapper">
+              <HeartIcon width={130} height={130} />
+            </div>
+            <p className="statistic_text description_text">
+              Щоденно годуємо понад 20 котів
+            </p>
+          </li>
+          <li className="statistic_item">
+            <div className="icon_wrapper">
+              <MoneyIcon width={130} height={130} />
+            </div>
+            <p className="description_text">
+              Щомісяця витрачаємо близько 24 000 грн
+            </p>
+          </li>
+          <li className="statistic_item">
+            <div className="icon_wrapper">
+              <HandsIcon width={130} height={130} />
+            </div>
+            <p className="description_text">
+              Понад 7 років постійно годуємо стабільний прайд
+            </p>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+export default StatisticDatas;
