c6b7db27b9d2df734538b8ce1208c01a5089e01c
Evgeniy <zhrkacomua@gmail.com>
2023-05-22 00:06:14 +0300

    add: cat profile page

--- client/src/pages/CatProfile/CatProfile.js
+++ client/src/pages/CatProfile/CatProfile.js
@@ -1,16 +1,29 @@
 import React, {useEffect, useState} from 'react';
-import Title from "../../components/Title/Title";
+import {Swiper} from "swiper/react";
 import {useParams} from "react-router-dom";
 import axios from "axios";
+import Title from "../../components/Title/Title";
 import './CatProfile.scss';
-import {Skeleton} from "@mui/material";
+import PersonalInfo from "./components/PersonalInfo";
+import paw from '../../assets/paw.svg';
+import calendar from '../../assets/calendar.svg';
+import infoRounded from '../../assets/info-rounded.svg';
+import CatsGallery from "../../components/CatsGallery/CatsGallery";
+ import 'swiper/css';
+
 
 const CatProfile = () => {
+    const initialPhotos = [
+        "https://s13.stc.yc.kpcdn.net/share/i/instagram/B44solahwlo/wr-1280.webp",
+        "https://c.files.bbci.co.uk/116AB/production/_115093317_neo4.jpg",
+        "https://kor.ill.in.ua/m/610x385/2229578.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/0/0e/Felis_silvestris_silvestris.jpg"
+    ];
     const {id} = useParams();
     const [cat, setCat] = useState(null);
-    console.log(id);
-    console.log(cat);
+    const [photos, setPhotos] = useState(initialPhotos);
     const getData = async () => {
+
         try {
             const response = await axios.get(`http://localhost:3000/cats/${id}`);
             setCat(response.data);
@@ -21,19 +34,58 @@ const CatProfile = () => {
     useEffect(() => {
         getData();
     }, []);
-
+    console.log(cat);
+    const handlePhotoClick = (index) => {
+        const updatedPhotos = [...photos];
+        const clickedPhoto = updatedPhotos.splice(index, 1);
+        updatedPhotos.unshift(clickedPhoto[0]);
+        setPhotos(updatedPhotos);
+    };
 
     return (
         <div>
             <Title text={cat?.name}/>
-           <div className='profile'>
-               <div className='profile__images'>
+            <div className="profile">
+                <section className="profile__wrapper">
+                    <div className="profile__wrapper_images">
 
-               </div>
-               <div className='profile__info'>
-                   <h2>Ти можеш допомогти {cat?.name}</h2>
-               </div>
-           </div>
+                        {photos.map((photo, index) => (
+                            <img
+                                key={index}
+                                src={photo}
+                                alt=""
+                                onClick={() => handlePhotoClick(index)}
+                            />
+                        ))}
+                    </div>
+                    <div className="profile__wrapper_info">
+                        <h2 className="info__title">Ти можеш
+                            допомогти <br/> {cat?.name}
+                        </h2>
+                        <PersonalInfo
+                            type={'Стать'}
+                            title={'кіт'}
+                            icon={paw}/>
+                        <PersonalInfo
+                            type={'Вік'}
+                            title={'5 років'}
+                            icon={calendar}/>
+                        <PersonalInfo
+                            type={'Наявність чіпа'}
+                            title={cat?.chipped}
+                            icon={infoRounded}/>
+                        <h3 className='info__subtitle'>Інформація</h3>
+                        <hr/>
+                        <p className='info__desc'>{cat?.description}</p>
+                        <button className='info__help'>допомогти</button>
+                    </div>
+                </section>
+                <section className='profile__others'>
+                    <h2 className='profile__others_title'>Інші пухнастики</h2>
+                    <CatsGallery/>
+
+                </section>
+            </div>
         </div>
     );
 };
