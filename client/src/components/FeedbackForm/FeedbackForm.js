74bd5d779cb06755f7a4a2c41f5501b3b8e694ec
olha-sydorenko <108460682+olha-sydorenko@users.noreply.github.com>
2023-05-22 13:44:34 +0300

    added validation

--- client/src/components/FeedbackForm/FeedbackForm.js
+++ client/src/components/FeedbackForm/FeedbackForm.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { Formik, Form, Field } from 'formik';
+import {feedbackSchema} from "./schema";
 import "./Feedbackform.scss"
 
 const FeedbackForm = () => {
     
-    const onSubmit = (values) => {
+    const onSubmit = async (values, {resetForm}) => {
+        await new Promise((resolve)=> setTimeout(resolve, 1000));
+        resetForm();
         console.log(values);
+        
     }
+
+
     return (
      <div className="feedback_wrapper">
          <h3 className="feedback_title">Зворотній зв’язок</h3>
          <p className="feedback_text">Якщо у Вас є запитання або пропозиції, напишіть нам</p>
-         
          
          <Formik
         initialValues={{
@@ -19,13 +24,19 @@ const FeedbackForm = () => {
           email: '',
           text: '',
         }}
-        onSubmit = {onSubmit}
-        
+        validationSchema={feedbackSchema}
+        onSubmit = {onSubmit}  
       >
+          {({ errors, touched }) => (
+        
          <Form className="feedback_form">
             <div className="input_wrapper">
             <label htmlFor="name" className="feedback_label">Імʼя</label>
-            <Field className="feedback_input" type="text" name="name" id="name"/>
+            <Field className={errors.name && touched.name ? "input_error" : "feedback_input"}
+             type="text" name="name" id="name"/>
+             {errors.name && touched.name && (
+                 <p className = "feedback_error">{errors.name}</p>
+             )}
             </div>
             <div className="input_wrapper">
             <label htmlFor="email" className="feedback_label">Електронна адреса</label>
@@ -37,6 +48,8 @@ const FeedbackForm = () => {
            </div>
            <button className="feedback_button"type="submit">Надіслати</button>
          </Form>
+         
+         )}
        </Formik>
      
      </div>   
