4081a4ad971f3a754de6dccbf8b0343aaebdb8c0
olha-sydorenko <108460682+olha-sydorenko@users.noreply.github.com>
2023-05-22 03:25:05 +0300

    Styled feedback form

--- client/src/components/FeedbackForm/FeedbackForm.js
+++ client/src/components/FeedbackForm/FeedbackForm.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { Formik, Form, Field } from 'formik';
+import "./Feedbackform.scss"
 
 const FeedbackForm = () => {
     
+    const onSubmit = (values) => {
+        console.log(values);
+    }
     return (
      <div className="feedback_wrapper">
          <h3 className="feedback_title">Зворотній зв’язок</h3>
@@ -15,16 +19,23 @@ const FeedbackForm = () => {
           email: '',
           text: '',
         }}
-        // onSubmit={onSubmit}
+        onSubmit = {onSubmit}
+        
       >
-         <Form>
-            <label htmlFor="name">Імʼя</label>
-            <Field type="text" name="name" id="name"/>
-            <label htmlFor="email">Електронна адреса</label>
-            <Field type="email" name="email" id="email"/>
-            <label htmlFor="name">Повідомлення</label>
-            <Field type="text" name="text" id="text"/>
-           <button type="submit">Submit</button>
+         <Form className="feedback_form">
+            <div className="input_wrapper">
+            <label htmlFor="name" className="feedback_label">Імʼя</label>
+            <Field className="feedback_input" type="text" name="name" id="name"/>
+            </div>
+            <div className="input_wrapper">
+            <label htmlFor="email" className="feedback_label">Електронна адреса</label>
+            <Field className="feedback_input" type="email" name="email" id="email"/>
+            </div>
+            <div className="input_wrapper">
+            <label htmlFor="text" className="feedback_label">Повідомлення</label>
+            <Field className="feedback_textarea" type="text" name="text" id="text" component="textarea"/>
+           </div>
+           <button className="feedback_button"type="submit">Надіслати</button>
          </Form>
        </Formik>
      
---
+++ client/src/pages/ContactsPage/ContactsPage 2.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Title from "../../components/Title/Title";
+import Contacts from "../../components/Contacts/Contacts";
+
+export const ContactsPage = () => {
+    return (
+    <>
+    <Title text="Контакти"/>
+    <Contacts />
+    </>    
+    );  
+};
