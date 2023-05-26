79a1ff3b7f7f871518c9ccc22ee504b44c34bd04
Tonya-Shyva <toshash69@gmail.com>
2023-05-26 21:27:21 +0300

    add global className = "container"

--- client/src/components/Donate/Donate.js
+++ client/src/components/Donate/Donate.js
@@ -4,7 +4,7 @@ import AccordionSlider from "../AccordionSlider/AccordionSlider";
 const SaveDonate = () => {
   return (
     <section className={"donate"}>
-      <div className={"container"}>
+      <div className={"donate_container"}>
         <div className={"donate-wrap"}>
           <h2 className={"donate-title"}>Безпечний донат</h2>
           <AccordionSlider />
