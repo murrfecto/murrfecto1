a27639a85afb84a4f1109f52d00de6d89b3474b5
Marina Kovaleva <marinakovaleva259@gmail.com>
2023-06-07 19:49:53 +0300

    Implement donate slider media and add portal to select

--- client/src/components/AccordionSlider/AccordionSliderMedia/AccordionSliderMedia.js
+++ client/src/components/AccordionSlider/AccordionSliderMedia/AccordionSliderMedia.js
@@ -1,21 +1,29 @@
-import './AccordionSliderMedia.scss';
-import { Accordion, AccordionItem } from '@szhsin/react-accordion';
-import DonateForm from '../../Donate/DonateForm/DonateForm';
-import HelpUs from '../../HelpUs/HelpUs';
+import "./AccordionSliderMedia.scss";
+import { Accordion, AccordionItem } from "@szhsin/react-accordion";
+import DonateForm from "../../Donate/DonateForm/DonateForm";
+import HelpUs from "../../HelpUs/HelpUs";
 
 const items = [
   {
-    header: 'Одноразово',
+    header: "Одноразово",
     content: (
-      <DonateForm title="Нагодуйте котів вже сьогодні - ваша допомога важлива" />
+      <DonateForm
+        title="Нагодуйте котів вже сьогодні - ваша допомога важлива"
+        optionIdPrefix="media-onetime"
+      />
     ),
   },
   {
-    header: 'Щомісячно',
-    content: <DonateForm title="Ваша  підтримка змінить життя одного кота!" />,
+    header: "Щомісячно",
+    content: (
+      <DonateForm
+        title="Ваша  підтримка змінить життя одного кота!"
+        optionIdPrefix="media-monthly"
+      />
+    ),
   },
   {
-    header: 'Кормом, ліками, іграшками',
+    header: "Кормом, ліками, іграшками",
     content: <HelpUs />,
   },
 ];
