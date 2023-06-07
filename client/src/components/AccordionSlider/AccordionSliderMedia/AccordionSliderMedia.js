663da3f790bfccd42c3a4e304b14aa443c347ed3
Marina Kovaleva <marinakovaleva259@gmail.com>
2023-06-07 16:17:28 +0300

    Install react-accordion and add data

---
+++ client/src/components/AccordionSlider/AccordionSliderMedia/AccordionSliderMedia.js
@@ -0,0 +1,35 @@
+import './AccordionSliderMedia.scss';
+import { Accordion, AccordionItem } from '@szhsin/react-accordion';
+import DonateForm from '../../Donate/DonateForm/DonateForm';
+import HelpUs from '../../HelpUs/HelpUs';
+
+const items = [
+  {
+    header: 'Одноразово',
+    content: (
+      <DonateForm title="Нагодуйте котів вже сьогодні - ваша допомога важлива" />
+    ),
+  },
+  {
+    header: 'Щомісячно',
+    content: <DonateForm title="Ваша  підтримка змінить життя одного кота!" />,
+  },
+  {
+    header: 'Кормом, ліками, іграшками',
+    content: <HelpUs />,
+  },
+];
+
+const AccordionSliderMedia = () => {
+  return (
+    <Accordion>
+      {items.map(({ header, content }, i) => (
+        <AccordionItem header={header} key={i}>
+          {content}
+        </AccordionItem>
+      ))}
+    </Accordion>
+  );
+};
+
+export default AccordionSliderMedia;
--- client/src/components/Donate/Donate.js
+++ client/src/components/Donate/Donate.js
@@ -1,13 +1,15 @@
-import "./Donate.scss";
-import AccordionSlider from "../AccordionSlider/AccordionSlider";
+import './Donate.scss';
+import AccordionSlider from '../AccordionSlider/AccordionSlider';
+import AccordionSliderMedia from '../AccordionSlider/AccordionSliderMedia/AccordionSliderMedia';
 
 const SaveDonate = () => {
   return (
-    <section className={"donate"}>
-      <div className={"donate_container"}>
-        <div className={"donate-wrap"}>
-          <h2 className={"donate-title"}>Безпечний донат</h2>
+    <section className={'donate'}>
+      <div className={'donate_container'}>
+        <div className={'donate-wrap'}>
+          <h2 className={'donate-title'}>Безпечний донат</h2>
           <AccordionSlider />
+          <AccordionSliderMedia />
         </div>
       </div>
     </section>
