b90cad94c633b0d8ecbee612c5abc2cda5ca4278
Marina Kovaleva <marinakovaleva259@gmail.com>
2023-06-12 21:20:54 +0300

    Add styles to front and back side on card

--- client/src/components/CatItem/CatItem.js
+++ client/src/components/CatItem/CatItem.js
@@ -1,61 +1,134 @@
-import React from 'react';
-import './CatItem.scss'
-import paw from '../../assets/paw.svg';
-import info from '../../assets/info-rounded.svg'
-import {Skeleton} from "@mui/material";
-import {Link} from "react-router-dom";
+import React from "react";
+import "./CatItem.scss";
+import paw from "../../assets/paw.svg";
+import info from "../../assets/info-rounded.svg";
+import { Skeleton } from "@mui/material";
+import { Link } from "react-router-dom";
 
-const CatItem = ({src, alt, name, description, chippedInfo, id, trash, select, gender, age}) => {
-    return (
-        <li className="cat_card" key={id}>
-            {select ? (<Link to={`/cat/${id}`}>
-                {src === undefined ? (
-                    <Skeleton variant="rect" style={{borderRadius: '20px'}} width={305} height={295}/>
-                ) : (
-                    <>
-                        {src && <img className="cat_card-icon" src={src} alt={alt}/>}
-                    </>
-                )}
-                <div className="cat_card-wrapper">
-                    <div className="cat_card-name">
-                        <p>{name}</p>
-                    </div>
-                    <div className="cat_card-description">
-                        <img src={paw} alt="paw"/>
-                        <p>{gender}, {age}</p>
-                    </div>
-                    <div className="cat_card-chipped">
-                        <img src={info} alt="info"/>
-                        <p>{chippedInfo}</p>
-                    </div>
-                    <div className="cat_card-trash">{trash}</div>
-                </div>
-            </Link>) : <div>
-                {src === undefined ? (
-                    <Skeleton variant="rect" style={{borderRadius: '20px'}} width={305} height={295}/>
-                ) : (
-                    <>
-                        {src && <img className="cat_card-icon" src={src} alt={alt}/>}
-                    </>
-                )}
-                <div className="cat_card-wrapper">
-                    <div className="cat_card-name">
-                        <p>{name}</p>
-                    </div>
-                    <div className="cat_card-description">
-                        <img src={paw} alt="paw"/>
-                        <p>{gender}, {age}</p>
-                    </div>
-                    <div className="cat_card-chipped">
-                        <img src={info} alt="info"/>
-                        <p>{chippedInfo}</p>
-                    </div>
-                    <div className="cat_card-trash">{trash}</div>
-                </div>
+const CatItem = ({
+  src,
+  alt,
+  name,
+  description,
+  chippedInfo,
+  id,
+  trash,
+  select,
+  gender,
+  age,
+}) => {
+  return (
+    <li className="cat_card" key={id}>
+      <div className="cat_card-front">
+        {select ? (
+          <Link to={`/cat/${id}`}>
+            {src === undefined ? (
+              <Skeleton
+                variant="rect"
+                style={{ borderRadius: "20px" }}
+                width={305}
+                height={295}
+              />
+            ) : (
+              <>
+                {src && <img className="cat_card-icon" src={src} alt={alt} />}
+              </>
+            )}
+            <div className="cat_card-wrapper">
+              <div className="cat_card-name">
+                <p>{name}</p>
+              </div>
+              <div className="cat_card-description">
+                <img src={paw} alt="paw" />
+                <p>
+                  {gender}, {age}
+                </p>
+              </div>
+              <div className="cat_card-chipped">
+                <img src={info} alt="info" />
+                <p>{chippedInfo}</p>
+              </div>
+              <div className="cat_card-trash">{trash}</div>
             </div>
-            }
-        </li>
-    );
+          </Link>
+        ) : (
+          <div>
+            {src === undefined ? (
+              <Skeleton
+                variant="rect"
+                style={{ borderRadius: "20px" }}
+                width={305}
+                height={295}
+              />
+            ) : (
+              <>
+                {src && <img className="cat_card-icon" src={src} alt={alt} />}
+              </>
+            )}
+            <div className="cat_card-wrapper">
+              <div className="cat_card-name">
+                <p>{name}</p>
+              </div>
+              <div className="cat_card-description">
+                <img src={paw} alt="paw" />
+                <p>
+                  {gender}, {age}
+                </p>
+              </div>
+              <div className="cat_card-chipped">
+                <img src={info} alt="info" />
+                <p>{chippedInfo}</p>
+              </div>
+              <div className="cat_card-trash">{trash}</div>
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="cat_card-back">
+        {select ? (
+          <Link to={`/cat/${id}`}>
+            {src === undefined ? (
+              <Skeleton
+                variant="rect"
+                style={{ borderRadius: "20px" }}
+                width={305}
+                height={295}
+              />
+            ) : (
+              <>
+                {src && <img className="cat_card-icon" src={src} alt={alt} />}
+              </>
+            )}
+            <div className="cat_card-wrapper">
+              <Link className="cat_card-link" to={`/cat/${id}`}>
+                Докладніше
+              </Link>
+            </div>
+          </Link>
+        ) : (
+          <div>
+            {src === undefined ? (
+              <Skeleton
+                variant="rect"
+                style={{ borderRadius: "20px" }}
+                width={305}
+                height={295}
+              />
+            ) : (
+              <>
+                {src && <img className="cat_card-icon" src={src} alt={alt} />}
+              </>
+            )}
+            <div className="cat_card-wrapper">
+              <Link className="cat_card-link" to={`/cat/${id}`}>
+                Докладніше
+              </Link>
+            </div>
+          </div>
+        )}
+      </div>
+    </li>
+  );
 };
 
 export default CatItem;
