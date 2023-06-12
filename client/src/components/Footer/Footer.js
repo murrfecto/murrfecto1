789e23489a80e05f47ef9f30da6add315715954b
Tonya-Shyva <toshash69@gmail.com>
2023-06-12 15:15:01 +0300

    Update Footer.js

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,4 +1,4 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./Footer.scss";
 import "../Header/Navbar/Navbar.scss";
 
@@ -7,134 +7,134 @@ import email from "../../assets/footer/envelope.svg";
 import phone from "../../assets/footer/phone.svg";
 import facebook from "../../assets/footer/facebook.svg";
 import insta from "../../assets/footer/insta.svg";
-import {scrollToTop} from "../../helpers/scrollToTop";
+import { scrollToTop } from "../../helpers/scrollToTop";
 import ModalButton from "../ModalButton/ModalButton";
 
 import PrivacyPolicyPdf from "../../assets/dataDocs/privacy-policy.pdf";
 
 const Footer = () => {
-    return (
-        <footer className={"footer"}>
-            <div className="footer_container container">
-                <div className={"footer_info_wrapper"}>
-                    <div className="wrapper">
-                        <Link to={"/"} className={"footer_logo-link"} onClick={scrollToTop}>
-                            <img
-                                className={"footer_logo-img"}
-                                src={logo}
-                                alt="murrfecto logo"
-                                width={170}
-                                height={70}
-                            />
-                        </Link>
+  return (
+    <footer className={"footer"}>
+      <div className="footer_container container">
+        <div className={"footer_info_wrapper"}>
+          <div className="wrapper">
+            <Link to={"/"} className={"footer_logo-link"} onClick={scrollToTop}>
+              <img
+                className={"footer_logo-img"}
+                src={logo}
+                alt="murrfecto logo"
+                width={170}
+                height={70}
+              />
+            </Link>
 
-                        <ModalButton title={"Нагодуй кота"}/>
-                    </div>
+            <ModalButton title={"Нагодуй кота"} />
+          </div>
 
-                    <nav className={"footer_nav"}>
-                        <Link className={"footer_link"} to={"/about"} onClick={scrollToTop}>
-                            Про притулок
-                        </Link>
-                        <Link to="/tails" className={"footer_link"} onClick={scrollToTop}>
-                            Наші хвости
-                        </Link>
-                    </nav>
+          <nav className={"footer_nav"}>
+            <Link className={"footer_link"} to={"/about"} onClick={scrollToTop}>
+              Про притулок
+            </Link>
+            <Link to="/tails" className={"footer_link"} onClick={scrollToTop}>
+              Наші хвости
+            </Link>
+          </nav>
 
-                    <address className="footer_address">
-                        <h2 className="contacts_title">Контакти</h2>
-                        <ul className="contacts_list">
-                            <li>
-                                <a className="info_link contacts_link" href="tel:+380636286630">
-                                    <img
-                                        className={"contacts_icon"}
-                                        src={phone}
-                                        alt="phone icon"
-                                    />
-                                    <span>+38 063 628 6630</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    className={"info_link  contacts_link"}
-                                    href="tel:+380675681788"
-                                >
-                                    <img
-                                        className={"contacts_icon"}
-                                        src={phone}
-                                        alt="phone icon"
-                                    />
-                                    <span>+38 067 568 1788</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    className={"info_link contacts_link"}
-                                    href="mailto:info@example.com"
-                                >
-                                    <img
-                                        className={"contacts_icon"}
-                                        src={email}
-                                        alt="email icon"
-                                    />
-                                    info@murrfecto.site
-                                </a>
-                            </li>
-                        </ul>
-                    </address>
+          <address className="footer_address">
+            <h2 className="contacts_title">Контакти</h2>
+            <ul className="contacts_list">
+              <li>
+                <a className="info_link contacts_link" href="tel:+380636286630">
+                  <img
+                    className={"contacts_icon"}
+                    src={phone}
+                    alt="phone icon"
+                  />
+                  <span>+38 063 628 6630</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  className={"info_link  contacts_link"}
+                  href="tel:+380675681788"
+                >
+                  <img
+                    className={"contacts_icon"}
+                    src={phone}
+                    alt="phone icon"
+                  />
+                  <span>+38 067 568 1788</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  className={"info_link contacts_link"}
+                  href="mailto:info@example.com"
+                >
+                  <img
+                    className={"contacts_icon"}
+                    src={email}
+                    alt="email icon"
+                  />
+                  info@murrfecto.site
+                </a>
+              </li>
+            </ul>
+          </address>
 
-                    <div className={"footer_documents-wrapper"}>
-                        <a
-                            className={"footer_link"}
-                            href="#"
-                            target="_blank"
-                            rel="nofollow noreferrer noopener"
-                        >
-                            Правила користування сайтом
-                        </a>
-                        <a
-                            className={"footer_link"}
-                            href={PrivacyPolicyPdf + "#toolbar=0"}
-                            target="_blank"
-                            aria-readonly
-                            rel="nofollow noreferrer noopener"
-                        >
-                            Політика конфіденційності
-                        </a>
-                    </div>
-                    <ul className={"social_list"}>
-                        <li className={"social_item"}>
-                            <a
-                                className={"social_link"}
-                                href="https://www.instagram.com/murrfecto_murr/"
-                                target="_blank"
-                                rel="nofollow noreferrer noopener"
-                            >
-                                <img src={insta} alt="instagramm" width={48} height={48}/>
-                            </a>
-                        </li>
-                        <li className={"social_item"}>
-                            <a
-                                className={"social_link"}
-                                href="https://www.facebook.com/profile.php?id=100092408450281"
-                                target="_blank"
-                                rel="nofollow noreferrer noopener"
-                            >
-                                <img
-                                    src={facebook}
-                                    alt="facebook icon"
-                                    width={48}
-                                    height={48}
-                                />
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <p className="footer_copyrights">
-                    Розробка BazaTraineeUkraine 2023 &#169; Всі права захищені.{" "}
-                </p>
-            </div>
-        </footer>
-    );
+          <div className={"footer_documents-wrapper"}>
+            <a
+              className={"footer_link"}
+              href="#"
+              target="_blank"
+              rel="nofollow noreferrer noopener"
+            >
+              Правила користування сайтом
+            </a>
+            <a
+              className={"footer_link"}
+              href={PrivacyPolicyPdf + "#toolbar=0"}
+              target="_blank"
+              aria-readonly
+              rel="nofollow noreferrer noopener"
+            >
+              Політика конфіденційності
+            </a>
+          </div>
+          <ul className={"social_list"}>
+            <li className={"social_item"}>
+              <a
+                className={"social_link"}
+                href="https://www.instagram.com/murrfecto_murr/"
+                target="_blank"
+                rel="nofollow noreferrer noopener"
+              >
+                <img src={insta} alt="instagramm" width={48} height={48} />
+              </a>
+            </li>
+            <li className={"social_item"}>
+              <a
+                className={"social_link"}
+                href="https://www.facebook.com/profile.php?id=100092408450281"
+                target="_blank"
+                rel="nofollow noreferrer noopener"
+              >
+                <img
+                  src={facebook}
+                  alt="facebook icon"
+                  width={48}
+                  height={48}
+                />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <p className="footer_copyrights">
+          Розробка Baza Trainee Ukraine 2023 Всі права захищені.
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
