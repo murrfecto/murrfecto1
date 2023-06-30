623347c45dfa800df02514e239be53eeb1f21fae
Marina Kovaleva <marinakovaleva259@gmail.com>
2023-06-30 17:29:13 +0300

    Add styles for media on not found page

--- client/src/components/NotFoundBox/NotFoundBox.js
+++ client/src/components/NotFoundBox/NotFoundBox.js
@@ -5,7 +5,7 @@ import img404 from "../../assets/404page/cat404.png";
 const NotFoundBox = () => {
   return (
     <main className="main_wrapper">
-      <div>
+      <div className="not-found">
         <h2 className={"not-found_title"}>404</h2>
         <p className="not-found_text">
           {" "}
@@ -16,7 +16,13 @@ const NotFoundBox = () => {
           На Головну
         </Link>
       </div>
-      <img src={img404} alt="grey cat" width={365} height={597} />
+      <img
+        className="not-found_img"
+        src={img404}
+        alt="grey cat"
+        width={365}
+        height={597}
+      />
     </main>
   );
 };
