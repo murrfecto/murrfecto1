0b59a38f8ff75e930d9e723b948b190bc74655d1
Evgeniy <zhrkacomua@gmail.com>
2023-07-09 14:17:40 +0300

    fix: add divider

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -15,7 +15,7 @@ const Header = () => {
           ):(
               <div className="header">
                   <Infobox />
-                  <hr/>
+
                   <Navbar />
               </div>
           )}
