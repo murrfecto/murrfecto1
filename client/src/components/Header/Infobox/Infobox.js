ac2f6eff3b9c3b4002391efaca129d579124abcc
# Ivan Myroniuk <95187372+MyroniukIvan@users.noreply.github.com>
2023-07-12 12:35:41 -0400

    fix: bug #29 MobileMenu - header prevents closing ModalContacts.js

--- client/src/components/Header/Infobox/Infobox.js
+++ client/src/components/Header/Infobox/Infobox.js
@@ -6,45 +6,45 @@ import email from "../../../assets/header/email.svg";
 import phone from "../../../assets/header/phone.svg";
 
 const Infobox = () => {
-  return (
-    <div className="header_infobox">
-      <ul className={"social_networks "}>
-        <li>
-          <a
-            href="https://www.instagram.com/murrfecto_murr/"
-            target="_blank"
-            rel="nofollow noreferrer noopener"
-          >
-            <Insta className="social_networks_icon"/>
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.facebook.com/profile.php?id=100092408450281"
-            target="_blank"
-            rel="nofollow noreferrer noopener"
-          >
-            <Facebook className="social_networks_icon"/>
-          </a>
-        </li>
-      </ul>
+    return (
+        <div className="header_infobox">
+            <ul className={"social_networks "}>
+                <li>
+                    <a
+                        href="https://www.instagram.com/murrfecto_murr/"
+                        target="_blank"
+                        rel="nofollow noreferrer noopener"
+                    >
+                        <Insta className="social_networks_icon"/>
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="https://www.facebook.com/profile.php?id=100092408450281"
+                        target="_blank"
+                        rel="nofollow noreferrer noopener"
+                    >
+                        <Facebook className="social_networks_icon"/>
+                    </a>
+                </li>
+            </ul>
 
-      <ul className={"socials_contacts"}>
-        <li className={"socials_contacts-item"}>
-          <p className={"socials_contacts-link"}>
-            <img src={email} alt="email-icon" />
-            <p>info@murrfecto.site</p>
-          </p>
-        </li>
-        <li className={"socials_contacts-item"}>
-          <p className={"socials_contacts-link"}>
-            <img src={phone} alt="phone-icon" />
-            <p>+38 063 628 6630</p>
-          </p>
-        </li>
-      </ul>
-    </div>
-  );
+            <ul className={"socials_contacts"}>
+                <li className={"socials_contacts-item"}>
+                    <p className={"socials_contacts-link"}>
+                        <img src={email} alt="email-icon"/>
+                        <p>info@murrfecto.site</p>
+                    </p>
+                </li>
+                <li className={"socials_contacts-item"}>
+                    <p className={"socials_contacts-link"}>
+                        <img src={phone} alt="phone-icon"/>
+                        <p>+38 063 628 6630</p>
+                    </p>
+                </li>
+            </ul>
+        </div>
+    );
 };
 
 export default Infobox;
--- client/src/components/ModalContacts/ModalContacts.js
+++ client/src/components/ModalContacts/ModalContacts.js
@@ -1,24 +1,24 @@
 import React from "react";
-import { createPortal } from "react-dom";
+import {createPortal} from "react-dom";
 import closeBtn from "../../assets/modal-close.svg";
 import "./ModalContacts.scss";
 
-const ModalContacts = ({ toggleModal }) => {
-  const modalRoot = document.getElementById("modal");
-  return createPortal (
-    <div className="modal_backdrop">
-      <div className="modal_container">
-        <button  id={'data-contacts'} type ="button" className="modal_btn_close" onClick={() => toggleModal()}>
-          <img className='modal_icon' src={closeBtn} alt="закриваюча кнопка" width={24} height={24} />
-        </button>
-        <h3 className="modal_title">Дякуємо!</h3>
-        <p className="modal_text">
-          Ваша допомога дуже важлива для наших хвостиків.
-        </p>
-      </div>
-    </div>,
-    modalRoot
-  );
+const ModalContacts = ({toggleModal}) => {
+    const modalRoot = document.getElementById("modal");
+    return createPortal(
+        <div className="modal_backdrop">
+            <div className="modal_container">
+                <button id="data-contacts" type="button" className="modal_btn_close" onClick={() => toggleModal()}>
+                    <img className="modal_icon" src={closeBtn} alt="закриваюча кнопка" width={24} height={24}/>
+                </button>
+                <h3 className="modal_title">Дякуємо!</h3>
+                <p className="modal_text">
+                    Ваша допомога дуже важлива для наших хвостиків.
+                </p>
+            </div>
+        </div>,
+        modalRoot
+    );
 };
 
 export default ModalContacts;
