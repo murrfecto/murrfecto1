8ce0456dd068fc3ac3d2e8371f1e4f5fb8768bc1
Evgeniy <zhrkacomua@gmail.com>
2023-07-24 23:54:54 +0300

    add: modal animation

--- client/src/components/ModalButton/ModalButton.js
+++ client/src/components/ModalButton/ModalButton.js
@@ -6,6 +6,7 @@ import DonateForm from "../Donate/DonateForm/DonateForm";
 import {TfiClose} from "react-icons/tfi";
 import {useDispatch, useSelector} from "react-redux";
 import {closeModal, openModal} from "../../store/modalSlice";
+import {motion} from "framer-motion";
 
 const ModalButton = ({title, style}) => {
     const isOpen = useSelector(state => state.modal.isOpen);
@@ -18,48 +19,88 @@ const ModalButton = ({title, style}) => {
     const handleCloseModal = () => {
         dispatch(closeModal()); // Диспетчер closeModal
     };
+
+
+
+    const modalVariant = {
+        initial: {
+            opacity: 0,
+            y: 50,
+            transition: {
+                type: 'spring',
+                duration: .5,
+                mass: 2,
+                damping: 10,
+                delay: .4
+            }
+        },
+        animate: {
+            opacity: 1,
+            y: 0,
+            transition: {
+                type: 'spring',
+                duration: .5,
+                mass: 2,
+                damping: 10,
+                delay: .2
+            }
+        },
+    }
     return (
         <div id={"donation"}>
             <a className={`navbar_links ${style}`}
                onClick={()=>handleOpenModal('onetime')}>
                 {title}
             </a>
-            <Modal
-                style={{
-                    overlay: {
-                        zIndex: 999,
-                        backgroundColor: "rgba(208, 190, 196, 0.8)",
-                        overflow: "auto",
-                    },
-                    content: {
-                        zIndex: 999,
-                        outline: "none",
-                    },
-                }}
-                className={"modal"}
-                isOpen={isOpen}
-                aria-label="Donate to feed a cat"
-                onRequestClose={() => {
-                    handleCloseModal();
-                }}
-                shouldCloseOnOverlayClick={true}
-            >
-                <div className={"donation__container"}>
-                    <img src={cats} alt={"donation__container_cats"}/>
-                </div>
-                <div className={"donation__container_inner"}>
-                    <TfiClose
-                        className={"donation__container_inner-close"}
-                        color="#4B3542"
-                        onClick={handleCloseModal}
-                    />
-                    <DonateForm
-                        optionIdPrefix="modal"
-                        title="Зібрані кошти підуть на харчування та медичну допомогу"
-                        hasDonateTypeButtons
-                    />
-                </div>
-            </Modal>
+
+                <Modal
+                    style={{
+                        overlay: {
+                            zIndex: 999,
+                            backgroundColor: "rgba(208, 190, 196, 0.8)",
+                            overflow: "auto",
+                        },
+                        content: {
+                            zIndex: 999,
+                            outline: "none",
+                        },
+                    }}
+                    closeTimeoutMS={800}
+                    className={"modal"}
+                    isOpen={isOpen}
+                    aria-label="Donate to feed a cat"
+                    onRequestClose={() => {
+                        handleCloseModal();
+                    }}
+                    shouldCloseOnOverlayClick={true}
+                >
+                    <motion.div
+                        className="box"
+                        variants={modalVariant}
+                        initial='initial'
+                        animate='animate'
+                        exit='initial'
+
+                    >
+                    <div className={"donation__container"}>
+                        <img src={cats} alt={"donation__container_cats"}/>
+                    </div>
+                    <div className={"donation__container_inner"}>
+                        <TfiClose
+                            className={"donation__container_inner-close"}
+                            color="#4B3542"
+                            onClick={handleCloseModal}
+                        />
+                        <DonateForm
+                            optionIdPrefix="modal"
+                            title="Зібрані кошти підуть на харчування та медичну допомогу"
+                            hasDonateTypeButtons
+                        />
+                    </div>
+                    </motion.div>
+                </Modal>
+
+
         </div>
     );
 };
